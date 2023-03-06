df5014ec39591bef7c2ba433bd00106d4c3597f4
Aivanas <vk53418006@gmail.com>
2023-03-07 00:11:51 +0300

    мгмнмм

--- Krestoviy Pohod/MainWindow.xaml.cs
+++ Krestoviy Pohod/MainWindow.xaml.cs
@@ -15,47 +15,121 @@ using System.Windows.Shapes;
 
 namespace Krestoviy_Pohod
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        bool is_end = false;
+        String player_side = "крестики";
+        int Clicked_buttons = 0;
+
+        void Win(System.Windows.Controls.Image arr_el )
+        {
+            clear_place();
+            if (arr_el == n_1 && player_side == "нолики")
+            {
+                MessageBox.Show("Вы выиграли :)");
+            }
+            else if (arr_el == k_1 && player_side == "крестики")
+            {
+                MessageBox.Show("Вы выиграли :)");
+            }
+            else if(arr_el == k_1 && player_side == "нолики")
+            {
+                MessageBox.Show("Вы проиграли :(");
+            }
+            else if (arr_el == n_1 && player_side == "крестики")
+            {
+                MessageBox.Show("Вы проиграли :(");
+            }
+
+        }
+        public void check_win(System.Windows.Controls.Image[] arr)
+        {
+            if (arr[0].Visibility == Visibility.Visible && arr[4].Visibility == Visibility.Visible && arr[8].Visibility == Visibility.Visible)
+            {
+                Win(arr[0]);
+                is_end= true;
+            }
+            else if (arr[0].Visibility == Visibility.Visible && arr[1].Visibility == Visibility.Visible && arr[2].Visibility == Visibility.Visible)
+            {
+                Win(arr[0]);
+                is_end = true;
+            }
+            else if (arr[0].Visibility == Visibility.Visible && arr[3].Visibility == Visibility.Visible && arr[6].Visibility == Visibility.Visible)
+            {
+                Win(arr[0]);
+                is_end = true;
+            }
+            else if (arr[1].Visibility == Visibility.Visible && arr[4].Visibility == Visibility.Visible && arr[7].Visibility == Visibility.Visible)
+            {
+                Win(arr[0]);
+                is_end = true;
+            }
+            else if (arr[2].Visibility == Visibility.Visible && arr[5].Visibility == Visibility.Visible && arr[8].Visibility == Visibility.Visible)
+            {
+                Win(arr[0]);
+                is_end = true;
+            }
+            else if (arr[2].Visibility == Visibility.Visible && arr[4].Visibility == Visibility.Visible && arr[6].Visibility == Visibility.Visible)
+            {
+                Win(arr[0]);
+                is_end = true;
+            }
+            else if (arr[6].Visibility == Visibility.Visible && arr[7].Visibility == Visibility.Visible && arr[8].Visibility == Visibility.Visible)
+            {
+                Win(arr[0]);
+                is_end = true;
+            }
+            else if (arr[3].Visibility == Visibility.Visible && arr[4].Visibility == Visibility.Visible && arr[5].Visibility == Visibility.Visible)
+            {
+                Win(arr[0]);
+                is_end = true;
+            }
+            else if (Clicked_buttons == 9)
+            {
+                //Случай если ничья
+                //не придумал
+            }
+
+            
+        }
 
 
-
-        public void mozg(Button my_button)
+        public void Show_clicked_button(int grid_num)
         {
             System.Windows.Controls.Image[] nol_arr = new[] { n_1, n_2, n_3, n_4, n_5, n_6, n_7, n_8, n_9 };
             System.Windows.Controls.Image[] kr_arr = new[] { k_1, k_2, k_3, k_4, k_5, k_6, k_7, k_8, k_9 };
-            int grid_num = Convert.ToInt16(my_button.Name.Substring(my_button.Name.Length - 1));
-            
-            Image img = nol_arr[grid_num - 1];
-            img.Visibility = Visibility.Visible;
-            
-
-            
-
-
-
+              
+            if (player_side == "крестики")
+            {
+                Clicked_buttons++;
+                Image img = kr_arr[grid_num - 1];
+                img.Visibility = Visibility.Visible;
+                check_win(kr_arr);
+                
+            }
+            else
+            {
+                Clicked_buttons++;
+                Image img = nol_arr[grid_num - 1];
+                img.Visibility = Visibility.Visible;
+                check_win(nol_arr);
+            }
+   
         }
 
         private void ButtonGrid_OnClick(object sender, RoutedEventArgs e)
         {
             Button my_button = sender as Button;
             my_button.IsEnabled = false;
-            mozg(my_button);
+            int grid_num = Convert.ToInt16(my_button.Name.Substring(my_button.Name.Length - 1));
+            Show_clicked_button(grid_num);
+            if (!is_end)
+            {
+                robo_step();
+            }
            
 
-
-            
-            //my_button.Background = new ImageBrush(new BitmapImage(uri));
-            //Uri uri = new Uri(System.IO.Path.GetFullPath(@"../../../Nolik.png"));
-            //DrillGrid.Children.Remove(my_button);
-            //Image my_image = sender as Image;
-            //my_image.Source = new BitmapImage(uri);
-            //MessageBox.Show(button.Grid.Column)
-
-            //DrillGrid.Children.Remove();
         }
 
 
@@ -66,9 +140,16 @@ namespace Krestoviy_Pohod
             {
                 btn.IsEnabled = true;
             });
+            Start_bt.IsEnabled = false;
+            is_end = false;
         }
 
         private void Rest_bt_Click(object sender, RoutedEventArgs e)
+        {
+            clear_place();
+        }
+
+        private void clear_place()
         {
             var buttons = DrillGrid.Children.OfType<Button>().ToList();
             buttons.ForEach(btn =>
@@ -76,23 +157,30 @@ namespace Krestoviy_Pohod
                 btn.IsEnabled = false;
             });
 
-            Button stButton = Start_bt;
-            stButton.IsEnabled = true;
-
-
+            Start_bt.IsEnabled = true;
 
             var images = DrillGrid.Children.OfType<Image>().ToList();
-            buttons.ForEach(img =>
+            images.ForEach(img =>
             {
                 img.Visibility = Visibility.Hidden;
             });
-            
-            
+        }
 
 
-
-
-
+        private void robo_step()
+        {
+            System.Windows.Controls.Image[] nol_arr = new[] { n_1, n_2, n_3, n_4, n_5, n_6, n_7, n_8, n_9 };
+            System.Windows.Controls.Image[] kr_arr = new[] { k_1, k_2, k_3, k_4, k_5, k_6, k_7, k_8, k_9 };
+            Random rand = new Random();
+            while (true)
+            {
+                int rand_num = rand.Next(0, 8);
+                if (nol_arr[rand_num].Visibility != Visibility.Visible && kr_arr[rand_num].Visibility != Visibility.Visible)
+                {
+                    Show_clicked_button(rand_num);
+                    break;
+                }
+            }
         }
     }
 }
